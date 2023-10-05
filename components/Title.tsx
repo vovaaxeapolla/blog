f13bfed8f145279b154ba2a424ad0797915df660
vovaaxeapolla <vovaaxeapolla@gmail.com>
2023-10-06 00:59:47 +0500

    CSS removing fix again 4

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,12 +1,10 @@
-import styles from '../styles/Title.module.sass';
-
 export default function Title({ text }) {
     return (
-        <h1 className={styles.title}>
-            <div className={styles['title__text']}>{
-                text.split('').map((l: string, i: number) => <span key={i} className={l === ' ' ? styles.space : ''}>{l}</span>)
+        <h1 className='title'>
+            <div className='title__text'>{
+                text.split('').map((l: string, i: number) => <span key={i} className={l === ' ' ? 'space' : ''}>{l}</span>)
             }</div>
-            <div className={styles['title__shadow']}>{text}</div>
+            <div className='title__shadow'>{text}</div>
         </h1>
     );
 }
