c5f25a0f0d25f921ddda789b7f78b575a98237f7
vovaaxeapolla <vovaaxeapolla@gmail.com>
2023-10-11 18:06:02 +0500

    transition

--- helpers/mapsAreEqual.ts
+++
@@ -1,3 +0,0 @@
-export function mapsAreEqual<Key, Value>(m1: Map<Key, Value>, m2: Map<Key, Value>): boolean {
-    return m1.size === m2.size && Array.from(m1.keys()).every((key) => m1.get(key) === m2.get(key));
-}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,9 @@ import type { AppProps } from "next/app";
 import { useEffect, createContext, useState } from "react";
 import Header from "../components/Header";
 import '../styles/global.sass';
-import { AnimatePresence, motion } from "framer-motion";
-import TilesTransition from "../components/TilesTransition";
 import Background from "../components/Background";
 import useVhFix from "../hooks/useVh";
 import ProgressBar from "../components/ProgressBar";
-import { mapsAreEqual } from "../helpers/mapsAreEqual";
 
 type Theme = "light" | "dark";
 type ThemeContext = { theme: Theme; toggleTheme: () => void };
@@ -15,33 +12,6 @@ type ThemeContext = { theme: Theme; toggleTheme: () => void };
 export const ThemeContext = createContext<ThemeContext>({} as ThemeContext);
 
 export default function App({ Component, pageProps, router }: AppProps) {
-
-    useEffect(() => {
-        const observer = new MutationObserver(mutationHandler);
-        observer.observe(document.head, { childList: true, subtree: true });
-
-        let stylesMap: Map<string, Node> = new Map();
-
-        function mutationHandler() {
-            const stylesNew: Map<string, Node> = new Map();
-            const styles = document.querySelectorAll('head > [data-n-href]');
-
-            [...styles].forEach(s => {
-                const clone = s.cloneNode(true) as Element;
-                clone.removeAttribute('media');
-                stylesNew.set(s.getAttribute('href') || '', clone)
-            });
-
-            if (!mapsAreEqual(stylesMap, stylesNew)){
-                stylesMap = stylesNew;
-                document.head.append(...stylesMap.values());
-            }
-        }
-
-        return () => {
-            observer.disconnect();
-        };
-    }, [])
 
     useVhFix();
 
@@ -59,23 +29,9 @@ export default function App({ Component, pageProps, router }: AppProps) {
         <ThemeContext.Provider value={{ theme, toggleTheme }}>
             <Header />
             <ProgressBar />
-            <AnimatePresence
-                initial={false}
-                mode='wait'
-                onExitComplete={() => window.scrollTo(0, 0)}
-            >
-                <motion.div
-                    key={router.asPath}
-                    transition={{ duration: .4 }}
-                    className="wrapper"
-                >
-                    <div className="wrapper">
-                        <Component {...pageProps} key={router.pathname} />
-                    </div>
-
-                    <TilesTransition TilesNumber={20} />
-                </motion.div>
-            </AnimatePresence>
+            <div className="wrapper">
+                <Component {...pageProps} key={router.pathname} />
+            </div>
             <Background />
         </ThemeContext.Provider>
     )
